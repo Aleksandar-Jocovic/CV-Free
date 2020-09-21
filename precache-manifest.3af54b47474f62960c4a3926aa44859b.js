efb246aed76032c1462735cb5e7a0f28b23e6042
aleksandar <jocovic099@gmail.com>
2020-09-21 21:24:01 +0200

    Updates

---
+++ precache-manifest.3af54b47474f62960c4a3926aa44859b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ae6be5af2dd3d6f0c79334a1a27493",
+    "url": "/CV-Free/index.html"
+  },
+  {
+    "revision": "134c0db5bdef00375004",
+    "url": "/CV-Free/static/css/main.33aa6c4d.chunk.css"
+  },
+  {
+    "revision": "89a9d61ce49e365e0f04",
+    "url": "/CV-Free/static/js/2.669c2e0d.chunk.js"
+  },
+  {
+    "revision": "ef1c1dc921eb892faf80c713a1f15928",
+    "url": "/CV-Free/static/js/2.669c2e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "134c0db5bdef00375004",
+    "url": "/CV-Free/static/js/main.4846bdb5.chunk.js"
+  },
+  {
+    "revision": "af285f57b98b5b5168c3",
+    "url": "/CV-Free/static/js/runtime-main.01cc74cf.js"
+  },
+  {
+    "revision": "4da2c76cc4c736591fa5c351be60c2af",
+    "url": "/CV-Free/static/media/pattern.4da2c76c.jpg"
+  }
+]);
